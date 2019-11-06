f46081e4304562f3ddb504622d25495fcd8a148b
Uthrom <>
2019-11-05 18:56:49 -0600

    Fixes again

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,6 +1,7 @@
 local function starts_with(str, start)
     return str:sub(1, #start) == start
 end
+
 for _, corpse in pairs(data.raw['corpse']) do
     if corpse.subgroup == 'corpses' then
         local monsterBody = table.deepcopy(data.raw['item']['coal'])
@@ -11,19 +12,15 @@ for _, corpse in pairs(data.raw['corpse']) do
         monsterBody.pictures = {{ filename = corpse.icon, mipmap_count = 1, scale = 1, size = corpse.icon_size}}
 
         if starts_with(corpse.name, "small") then
-	    monsterBody.name = "small-biter-corpse-burnt"
 	    monsterBody.fuel_value = "4MJ"
 	end
         if starts_with(corpse.name, "medium") then 
-	    monsterBody.name = "medium-biter-corpse-burnt"
 	    monsterBody.fuel_value = "8MJ" 
 	end
         if starts_with(corpse.name, "big") then 
-	    monsterBody.name = "big-biter-corpse-burnt"
 	    monsterBody.fuel_value = "16MJ" 
 	end
         if starts_with(corpse.name, "behemoth") then 
-	    monsterBody.name = "behemoth-biter-corpse-burnt"
 	    monsterBody.fuel_value = "32MJ" 
 	end
         
