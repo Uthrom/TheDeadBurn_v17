d2ccaeec4eeac5fc144bb51d20d15578fb4a37d5
billbo99 <cactus_computers-github@yahoo.com>
2020-01-08 16:48:17 +0000

    Fixes: - Found where the rouge 'behemoth' bosy was coming from - Added tints to the different bodies to make them easier to tell apart

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -2,31 +2,38 @@ local function starts_with(str, start)
     return str:sub(1, #start) == start
 end
 
-for _, corpse in pairs(data.raw['corpse']) do
-    if corpse.subgroup == 'corpses' then
-        local monsterBody = table.deepcopy(data.raw['item']['coal'])
+for _, corpse in pairs(data.raw["corpse"]) do
+    if corpse.subgroup == "corpses" then
+        local tint
+        local monsterBody = table.deepcopy(data.raw["item"]["coal"])
         monsterBody.name = "monster-body"
         monsterBody.icon = "__base__/graphics/icons/medium-biter-corpse.png"
         monsterBody.icon_size = 32
-        monsterBody.icon_mipmaps = 1
-        monsterBody.pictures = {{ filename = monsterBody.icon, mipmap_count = 1, scale = 1, size = corpse.icon_size}}
 
         if starts_with(corpse.name, "small") then
-	    monsterBody.name = "monster-body-small"
-	    monsterBody.fuel_value = "4MJ"
-        elseif starts_with(corpse.name, "medium") then 
-	    monsterBody.name = "monster-body-medium"
-	    monsterBody.fuel_value = "64MJ" 
-        elseif starts_with(corpse.name, "big") then 
-	    monsterBody.name = "monster-body-big"
-	    monsterBody.fuel_value = "128MJ" 
-        elseif starts_with(corpse.name, "behemoth") then 
-	    monsterBody.name = "monster-body-behemoth"
-	    monsterBody.fuel_value = "256MJ" 
+            monsterBody.name = "monster-body-small"
+            monsterBody.fuel_value = "4MJ"
+        elseif starts_with(corpse.name, "medium") then
+            monsterBody.name = "monster-body-medium"
+            tint = {r = 255, g = 0, b = 0}
+            monsterBody.fuel_value = "64MJ"
+        elseif starts_with(corpse.name, "big") then
+            monsterBody.name = "monster-body-big"
+            tint = {r = 0, g = 0, b = 255}
+            monsterBody.fuel_value = "128MJ"
+        elseif starts_with(corpse.name, "behemoth") then
+            monsterBody.name = "monster-body-behemoth"
+            tint = {r = 0, g = 255, b = 0}
+            monsterBody.fuel_value = "256MJ"
         else
             monsterBody.name = "monster-body-small"
             monsterBody.fuel_value = "4MJ"
         end
+
+        monsterBody.icon_mipmaps = 1
+        monsterBody.icons = {{icon = monsterBody.icon, icon_size = 32, tint = tint}}
+        monsterBody.pictures = {{filename = monsterBody.icon, tint = tint, mipmap_count = 1, scale = 1, size = corpse.icon_size}}
+
         data:extend({monsterBody})
     end
 end
